b2fea1e9ea22d55e16f09d56ee1dae427273ad80
Ricardo Rocha <ricardo8610@gmail.com>
2024-10-11 06:49:10 -0300

    Update mate56-materiais.py

--- paginas/mate56-materiais.py
+++ paginas/mate56-materiais.py
@@ -22,8 +22,8 @@ st.write('#### Aula 2 - 11/10/2024')
 c1, c2 = st.columns(2)
 
 with c1:
-	colab = 'https://colab.research.google.com/drive/1kvNAWirEsw20pr3qMicVDCKUvv5JbXyg?usp=sharing'
-	st.link_button('**👨‍💻 Script Google Colab**', url = colab,  help=None, type="primary", disabled=True, use_container_width=True)
+	colab = 'https://colab.research.google.com/drive/19TJ3Vme9w60NY0W58STBnYVUCmREx92q?usp=sharing'
+	st.link_button('**👨‍💻 Script Google Colab**', url = colab,  help=None, type="primary", disabled=False, use_container_width=True)
 
 with c2:
 	slides = 'https://drive.google.com/file/d/12ITiEyXHnSkmNCdPfTNWeLUEBQpHKZbJ/view?usp=sharing'
