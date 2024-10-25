319b09303576c328a5db38d84b719e45aacbfb56
Ricardo Rocha <ricardo8610@gmail.com>
2024-10-24 23:53:00 -0300

    Update mate56-materiais.py

--- paginas/mate56-materiais.py
+++ paginas/mate56-materiais.py
@@ -2,8 +2,10 @@ import streamlit as st
   
 st.title('📚 Materiais')   
 
+
+st.write('#### Repositório Github desse Aplicativo')
 git = 'https://github.com/ricardorocha86/mate56'
-st.link_button('**📂 Github desse App**', url = git,  help=None, type="primary", disabled=False, use_container_width=False)
+st.link_button('**📂 Github**', url = git,  help=None, type="primary", disabled=False, use_container_width=False)
 
 
 st.write('#### Minicurso Python para IA')
@@ -25,28 +27,13 @@ with c2:
  
 
 st.write('#### Aula 2 - 11/10/2024')
-c1, c2 = st.columns(2)
-
-with c1:
-	colab = 'https://colab.research.google.com/drive/19TJ3Vme9w60NY0W58STBnYVUCmREx92q?usp=sharing'
-	st.link_button('**👨‍💻 Script Google Colab**', url = colab,  help=None, type="primary", disabled=False, use_container_width=True)
-
-with c2:
-	slides = 'https://drive.google.com/file/d/12ITiEyXHnSkmNCdPfTNWeLUEBQpHKZbJ/view?usp=sharing'
-	st.link_button('**Slides**', url = slides,  help=None, type="primary", disabled=True, use_container_width=True)
-
+colab = 'https://colab.research.google.com/drive/19TJ3Vme9w60NY0W58STBnYVUCmREx92q?usp=sharing'
+st.link_button('**👨‍💻 Script Google Colab**', url = colab,  help=None, type="primary", disabled=False, use_container_width=True)
 
 
 st.write('#### Aula 3 - 18/10/2024')
-c1, c2 = st.columns(2)
-
-with c1:
-	colab = 'https://colab.research.google.com/drive/1YSe6mhROB0aTK394kORESVmDXpUa-a9A?usp=sharing'
-	st.link_button('**👨‍💻 Script Google Colab**', url = colab,  help=None, type="primary", disabled=False, use_container_width=True)
-
-with c2:
-	slides = 'https://drive.google.com/file/d/12ITiEyXHnSkmNCdPfTNWeLUEBQpHKZbJ/view?usp=sharing'
-	st.link_button('**Slides**', url = slides,  help=None, type="primary", disabled=True, use_container_width=True)
+colab = 'https://colab.research.google.com/drive/1YSe6mhROB0aTK394kORESVmDXpUa-a9A?usp=sharing'
+st.link_button('**👨‍💻 Script Google Colab**', url = colab,  help=None, type="primary", disabled=False, use_container_width=False) 
 
 
 st.write('#### Aula 4 - 25/10/2024')
